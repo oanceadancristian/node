1143913180d03f0a419924949287d317d47514bb
Dan Cristian Oancea <oanceadancristian@gmail.com>
2023-07-14 15:30:41 +0300

    section 6

---
+++ section6/assignment/app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+
+const app = express();
+const bodyParser = require('body-parser');
+const expressHbs = require('express-handlebars');
+
+const users = [];
+
+app.set('view engine', 'ejs');
+// app.engine('hbs', expressHbs({ defaultLayout: 'main-layout', extname: 'hbs' }));
+// app.set('view engine', 'hbs');
+// app.set('view engine', 'pug');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('/', (req, res, next) => {
+  res.render('index', { pageTitle: 'Add User' });
+});
+
+app.get('/users', (req, res, next) => {
+  res.render('users', {
+    pageTitle: 'Users',
+    users: users,
+    hasUsers: users.length > 0,
+  });
+});
+
+app.post('/add-user', (req, res, next) => {
+  users.push({ name: req.body.username });
+  res.redirect('/users');
+});
+
+app.listen(3000);
