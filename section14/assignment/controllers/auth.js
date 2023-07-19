37afda9230483b2f82a86978945a2f06b4ac5f1d
Dan Cristian Oancea <oanceadancristian@gmail.com>
2023-07-19 17:24:59 +0300

    section 14

---
+++ section14/assignment/controllers/auth.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+
+exports.getLogin = (req, res, next) => {
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    isAuthenticated: false,
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  User.findById('64b7b97511d0177b9b2ff7b6')
+    .then((user) => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch((err) => console.log(err));
+};
