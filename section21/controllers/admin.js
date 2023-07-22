436d5f88f1322185e9ebd1aeb6dbfaece35092f9
Dan Cristian Oancea <oanceadancristian@gmail.com>
2023-07-22 22:10:38 +0300

    section 22

--- section21/controllers/admin.js
+++ section21/controllers/admin.js
@@ -22,39 +22,39 @@ exports.postAddProduct = (req, res, next) => {
   const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
-  // if (!image) {
-  //   return res.status(422).render('admin/edit-product', {
-  //     pageTitle: 'Add Product',
-  //     path: '/admin/add-product',
-  //     editing: false,
-  //     hasError: true,
-  //     product: {
-  //       title: title,
-  //       price: price,
-  //       description: description,
-  //     },
-  //     errorMessage: 'Attached file is not an image.',
-  //     validationErrors: [],
-  //   });
-  // }
-  // const errors = validationResult(req);
+  if (!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      },
+      errorMessage: 'Attached file is not an image.',
+      validationErrors: [],
+    });
+  }
+  const errors = validationResult(req);
 
-  // if (!errors.isEmpty()) {
-  //   console.log(errors.array());
-  //   return res.status(422).render('admin/edit-product', {
-  //     pageTitle: 'Add Product',
-  //     path: '/admin/add-product',
-  //     editing: false,
-  //     hasError: true,
-  //     product: {
-  //       title: title,
-  //       price: price,
-  //       description: description,
-  //     },
-  //     errorMessage: errors.array()[0].msg,
-  //     validationErrors: errors.array(),
-  //   });
-  // }
+  if (!errors.isEmpty()) {
+    console.log(errors.array());
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description,
+      },
+      errorMessage: errors.array()[0].msg,
+      validationErrors: errors.array(),
+    });
+  }
 
   const imageUrl = image.path;
 
