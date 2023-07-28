43c2198aa50f7e30dda2de3e72545b9712befdbf
Dan Cristian Oancea <oanceadancristian@gmail.com>
2023-07-29 00:16:07 +0300

    section 23

--- section23/app.js
+++ section23/app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
 const csrf = require('csurf');
-const flash = require('connect-flash');
+// const flash = require('connect-flash');
 const multer = require('multer');
 
 const errorController = require('./controllers/error');
